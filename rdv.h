674a086b848ba8111582c7870c5b9edcec28f53f
MohamedAmineBelHadjSalah <mohamedamine.belhadjsalah@esprit.tn>
2023-03-15 18:46:40 +0100

    ok

--- rdv.h
+++ rdv.h
@@ -1,17 +1,16 @@
-
-
 #ifndef RDV_H
 #define RDV_H
 #include <QDate>
 #include<QString>
 #include<QSqlQuery>
+#include <QSqlTableModel>
 #include<QSqlQueryModel>
 #include <QCalendarWidget>
 using namespace std;
 
 
 class rdv
-{
+{private:
     QString adresse_visite,date_visite,heure_visite;
     int num_visite,etat,id,matricule;
 
@@ -19,36 +18,47 @@ class rdv
 public:
     //Constructeurs
     rdv();
-    rdv(int,int,QString,int,int,QString,QString);
+    rdv(int,int,QString,QString,int,QString,int);
 
     //Getters
-    QString getadresse_visite(){return adresse_visite;}
+    int getid(){return id;}
+    int getmatricule(){return matricule;}
     QString getdate_visite(){return date_visite;}
     QString getheure_visite(){return heure_visite;}
-    int getmatricule(){return matricule;}
     int getnum_visite(){return num_visite;}
+    QString getadresse_visite(){return adresse_visite;}
     int getetat(){return etat;}
-    int getid(){return id;}
+
 
     //Setters
-    void setadresse_visite(QString a){adresse_visite=a;}
+    void setid(int id){this->id=id;}
+    void setmatricule(int m){matricule=m;}
     void setdate_visite(QString d){date_visite=d;}
     void setheure_visite(QString h){heure_visite=h;}
-    void setmatricule(int m){matricule=m;}
     void setnum_visite(int nv){this->num_visite=nv;}
+    void setadresse_visite(QString a){adresse_visite=a;}
     void setetat(int e){this->etat=e;}
-    void setid(int id){this->id=id;}
+
 
 
     //Fonctionnalités de base
     bool ajouter();
+
     QSqlQueryModel * afficher_rdv();
     bool supprimer(int);
     bool modifier();
     QSqlQueryModel*chercher_rdv(QString rech);
     QSqlQueryModel * remplircombo();
     QSqlQueryModel * remplircombo1();
-    bool modifier(int,int,QString,int,int,QString,QString);
+    QSqlQueryModel * remplircombosupp();
+    bool modifier(int,int,QString,QString,QString,int,int);
+    QSqlQueryModel *trid(QString a);
+     QSqlQueryModel * afficherecherche(QString);
+     QSqlQuery trouver_num_visite(int);
+     int calculAdresse_visite(QString Adresse_visite);
+     QStringList listAdresse_visite();
+
+
 };
 
 #endif // RDV_H
